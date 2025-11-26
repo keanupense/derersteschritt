066daedf897fef1d243988bea2f3f0a9c47f9250
keanupense <keanu.pense04@gmail.com>
2025-11-26 14:15:42 +0100

    Add files via upload

---
+++ data/groups.js
@@ -0,0 +1,24 @@
+// data/groups.js
+const GROUPS = [
+  {
+    name: "AA Kreuzberg",
+    type: "sucht", // 'sucht' oder 'psychisch'
+    focus: "Alkoholsucht",
+    district: "Kreuzberg",
+    weekday: "Montag",
+    time: "19:00",
+    address: "Beispielstraße 1, 10997 Berlin",
+    link: "https://example-aa-berlin.de"
+  },
+  {
+    name: "Depressionsgruppe Mitte",
+    type: "psychisch",
+    focus: "Depression",
+    district: "Mitte",
+    weekday: "Mittwoch",
+    time: "18:30",
+    address: "Musterweg 5, 10115 Berlin",
+    link: ""
+  }
+  // Hier kannst du später weitere Gruppen ergänzen
+];
